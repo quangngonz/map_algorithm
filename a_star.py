b4a1a097c1f13002ad68d73c036041c7a35fb77d
Quang Ngo <46818455+quangngonz@users.noreply.github.com>
2025-02-19 14:03:10 +0700

    Update README and modify A* algorithm to use random end node

--- a_star.py
+++ a_star.py
@@ -1,5 +1,6 @@
 from unit_test.generate_graph import generate_data
 from unit_test.result import a_star_test
+import random
 
 G = generate_data()
 
@@ -7,8 +8,11 @@ print(G)
 results = {}
 
 BASE_NODE = 0
-END_NODE = max(G['nodes'])
+END_NODE = random.choice(G['nodes'])
+
+print(f"Start node: {BASE_NODE}")
+print(f"End node: {END_NODE}")
 
 # Implement A* pathfinding algorithm here
 
-path, length = a_star_test(G, 0, 5, plot_expected_result=True)
+path, length = a_star_test(G, 0, END_NODE, plot_expected_result=True)
